6b65ed4256d02afe72b7c1a00d7efe77d04d2d0f
Alexander Antonenko <mercurialf3@gmail.com>
2023-03-27 14:19:11 +0300

    Added methods to search for an already existing record and search for all records by a given pattern.

--- src/main/java/database/DataBaseConnector.java
+++ src/main/java/database/DataBaseConnector.java
@@ -1,6 +1,7 @@
 package database;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DataBaseConnector
 {
@@ -16,7 +17,32 @@ public class DataBaseConnector
         }
     }
 
-    public void addNewExpression(String expression) {
+    private boolean checkExistsExpression(String expression) {
+        boolean searchResult = false;
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement statement = connection.createStatement();
+            String query = "select * from math_expression.expression where expression='" + expression + "'";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            searchResult = resultSet.next();
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return searchResult;
+    }
+
+    public String addNewExpression(String expression) {
+        String operationResult = "";
+        if (checkExistsExpression(expression))
+            return "The expression already exists in the database";
+
         try {
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             String query = "insert into math_expression.expression (expression) value (?);";
@@ -24,10 +50,38 @@ public class DataBaseConnector
 
             statement.setString(1, expression);
             statement.executeUpdate();
+            operationResult += "Expression added to database";
+
             statement.close();
             connection.close();
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
+        return operationResult;
+    }
+
+    public ArrayList<String> getExpressions(String searchString) {
+        ArrayList<String> result = new ArrayList<>();
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement preparedStatement = connection.
+                    prepareStatement("select * from expression where expression like ?");
+            preparedStatement.setString(1, "%" + searchString + "%");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String expression = resultSet.getString("expression");
+                result.add(expression);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return result;
     }
 }
